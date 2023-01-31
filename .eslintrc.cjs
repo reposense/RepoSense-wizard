8a554d64fc1415fe3266a185d61703adeded96d2
vvidday <davids89034@gmail.com>
2023-01-31 21:53:38 +0800

    Install eslint-plugin-vue-pug to apply vue-eslint rules to pug templates

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
+    'plugin:vue-pug/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript'
   ],
