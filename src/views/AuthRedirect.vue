e4222bf46d8f1e2798ee6eda97b0c0916bd22162
David Ong <45852430+vvidday@users.noreply.github.com>
2023-02-11 20:44:56 +0800

    Consolidate variables into public env file (#2)
    Currently, certain deployment-reliant variables are being stored in
    code as constants, despite being subject to change. Also, despite it
    not containing sensitive information, the current .env file is being
    ignored by version control, which makes it troublesome to change the
    variables due to GitHub permissions.
    
    Let's shift the deployment-reliant variables to a .env file to
    consolidate them. Additionally, let's add this .env file to version
    control to simplify their changing process.

--- src/views/AuthRedirect.vue
+++ src/views/AuthRedirect.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 
-const CALLBACK_URL_DEV = 'http://localhost:5173/auth';
+/*
+  This is a workaround for GitHub only allowing one redirect URL per app.
+  During development, in order to redirect to localhost after auth, we first redirect
+  to this page (/auth/redirect) on the hosted site, which then redirects to localhost.
+*/
+
+const REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL as string;
 
 onMounted(() => {
   const query = window.location.search;
-  window.location.href = CALLBACK_URL_DEV + query;
+  window.location.href = REDIRECT_URL + query;
 })
 </script>
 
